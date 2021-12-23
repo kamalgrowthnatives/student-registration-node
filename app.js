e55c05a7d1a41fc52762d8b01c6e34979fa55317
Kamal <kamal.kashyap@growthnatives.com>
2021-12-23 13:51:29 +0530

    kk

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.set("view engine", "ejs");
 require("./middlewares");
 require("./modules");
 
-require("./services/startupService").initializeServer();
+require("./services/startupService").initializer();
